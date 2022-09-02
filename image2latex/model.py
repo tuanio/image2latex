4ef0436d90d6989646e566f1488f2059f189f593
Nguyen Van Anh Tuan <nvatuan3@gmail.com>
2022-09-02 15:33:10 +0700

    edit a new_tensor

--- image2latex/model.py
+++ image2latex/model.py
@@ -92,8 +92,9 @@ class Image2Latex(nn.Module):
         predict = []
 
         o = None
-        _input = torch.zeros(bs) + self.decoder.sos_id
-        _input = _input.to(dtype=torch.long)
+        _input = x.new_tensor(torch.zeros(bs) + self.decoder.sos_id).to(
+            dtype=torch.long
+        )
         for t in range(1, max_length):
             _input = _input.unsqueeze(1)
 
--- image2latex/text.py
+++ image2latex/text.py
@@ -3,18 +3,22 @@ import re
 import torch
 from torch import Tensor
 
+
 class Text:
     def __init__(self):
-        self.tokens = json.load(open('data/latex_tokens.json', 'r'))
+        self.tokens = json.load(open("data/latex_tokens.json", "r"))
         self.map_tokens = dict(zip(self.tokens, range(len(self.tokens))))
         self.pad_id = 0
         self.sos_id = 1
         self.eos_id = 2
-        self.TOKENIZE_PATTERN = re.compile("(\\\\[a-zA-Z]+)|"+ # \[command name]
-                              #"(\{\w+?\})|"+ # {[text-here]} Check if this is needed
-                              "((\\\\)*[$-/:-?{-~!\"^_`\[\]])|"+ # math symbols
-                              "(\w)|"+ # single letters or other chars
-                              "(\\\\)") # \ characters
+        self.TOKENIZE_PATTERN = re.compile(
+            "(\\\\[a-zA-Z]+)|"
+            +  # \[command name]
+            # "(\{\w+?\})|"+ # {[text-here]} Check if this is needed
+            '((\\\\)*[$-/:-?{-~!"^_`\[\]])|'
+            + "(\w)|"  # math symbols
+            + "(\\\\)"  # single letters or other chars
+        )  # \ characters
 
     def tokenize(self, formula: str):
         """Returns list of tokens in given formula.
@@ -26,7 +30,7 @@ class Text:
         return tokens
 
     def int2text(self, x: Tensor):
-        return ''.join([self.tokens[i] for i in x if i > self.eos_id])
-    
+        return "".join([self.tokens[i] for i in x if i > self.eos_id])
+
     def text2int(self, formula: str):
         return torch.LongTensor([self.map_tokens[i] for i in self.tokenize(formula)])
