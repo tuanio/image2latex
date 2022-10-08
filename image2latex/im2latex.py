544d431cb447ad592610fbcccf1055bef833ee68
Nguyen Van Anh Tuan <nvatuan3@gmail.com>
2022-10-08 18:36:05 +0700

    eddit beamsearch

--- image2latex/im2latex.py
+++ image2latex/im2latex.py
@@ -73,7 +73,7 @@ class Image2Latex(nn.Module):
         predict = []
         if self.decode_type == "greedy":
             predict = self.decode_greedy(x, max_length)
-        elif self.decode_type == "beam_search":
+        elif self.decode_type == "beamsearch":
             predict = self.decode_beam_search(x, self.beam_width, max_length)
         return self.text.int2text(predict)
 
